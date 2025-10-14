439da805dc3be7bb7e98385713e877c7f58b5143
Drongo sas <148218896+drongo-sas@users.noreply.github.com>
2025-10-14 17:37:55 +0200

    Update content: 1 edit(s) across 1 component(s) (link)

--- src/components/sections/home/Hero.tsx
+++ src/components/sections/home/Hero.tsx
@@ -20,7 +20,7 @@ const DEFAULT_HERO = {
   ],
   primaryCTA: 'Start NOw',
   secondaryCTA: 'Watch Demo',
-  primaryCTAHref: '#pricing',
+  primaryCTAHref: 'https://dash.cloudflare.com/',
   secondaryCTAHref: '#demo',
   imageUrl:
     'https://images.unsplash.com/photo-1551434678-e076c223a692?q=80&w=1920&auto=format&fit=crop',
